98a893287bbe97084158a3880225b433ae499360
BraHim WaHman <brahim@BraHims-MacBook-Pro.local>
2023-06-12 16:40:43 +0100

    first commit

---
+++ src/app/client-welcome/client-welcome.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientWelcomeComponent } from './client-welcome.component';
+
+describe('ClientWelcomeComponent', () => {
+  let component: ClientWelcomeComponent;
+  let fixture: ComponentFixture<ClientWelcomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClientWelcomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientWelcomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/client-welcome/client-welcome.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-welcome',
+  templateUrl: './client-welcome.component.html',
+  styleUrls: ['./client-welcome.component.css']
+})
+export class ClientWelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
